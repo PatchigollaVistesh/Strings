0b24b32e70a0a25ab0d98829dea405441bcf60f8
PatchigollaVistesh <patchigollavistesh@gmail.com>
2023-09-14 23:55:53 +0530

    using SortedMap

---
+++ src/com/string/PrintDuplicatesCount.java
@@ -0,0 +1,26 @@
+package com.string;
+
+import java.util.*;
+
+public class PrintDuplicatesCount {
+	public static void duplicateCount(String word) {
+		SortedMap<Character, Integer> chars = new TreeMap<>();
+		for (int i = 0; i < word.length(); i++) {
+			if (chars.containsKey(word.charAt(i))) {
+				chars.put(word.charAt(i),chars.get(word.charAt(i))+1);
+			}
+			else {
+				chars.put(word.charAt(i), 1);
+			}
+		}
+		System.out.println(chars);
+
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word = sc.next();
+		duplicateCount(word);
+	}
+
+}
